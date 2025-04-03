7434ea06741aa8c762a41a46a0e4a1da8ef67a28
moplatt <moritz.plattner03@gmail.com>
2025-04-03 17:48:23 +0200

    layercontrol mit 4 overlays

--- main.js
+++ main.js
@@ -11,17 +11,17 @@ let stephansdom = {
 // Karte initialisieren
 let map = L.map("map").setView([stephansdom.lat, stephansdom.lng], stephansdom.zoom);
 
-// Hintergrundkarte definieren
-L.tileLayer('https://mapsneu.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png', {
-    maxZoom: 19,
-    attribution: 'Hintergrundkarte: <a href="https://www.basemap.at">basemap.at</a>'
-}).addTo(map);
-
 // Layercontrol
 L.control.layers({
-
+    "BasemapAT grau": L.tileLayer('https://mapsneu.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png', {
+    maxZoom: 19,
+    attribution: 'Hintergrundkarte: <a href="https://www.basemap.at">basemap.at</a>'
+}).addTo(map)
 }, {
-
+    "Sehenswürdigkeiten": L.featureGroup().addTo(map), // .addto(map) um layer default zu checken
+    "Vienna sightseeing Linien": L.featureGroup().addTo(map),
+    "Vienna sightseeing Haltestellen": L.featureGroup().addTo(map),
+    "Fußgängerzonen": L.featureGroup().addTo(map),
 }).addTo(map);
 
 // Maßstab
